7b2138e01a3a005be4a2b42d2ed3605319eeac83
Stefcho1227 <stefan.ivanov1227@gmail.com>
2025-04-25 13:09:52 +0300

    new

---
+++ projectNotes/src/main/java/org/example/projectnotes/ProjectNotesApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectNotesApplication.class, args);
+    }
+
+}
